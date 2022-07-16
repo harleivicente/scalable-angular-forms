ad7b590499e563145f9dff1f1b136b66a2c82fcf
Harlei Vicente <harleivicente@gmail.com>
2022-07-15 22:17:34 -0300

    initial commit

--- apps/ui/src/app/app.component.ts
+++ apps/ui/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'pct-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ui';
+  submitted = false;
+
+  formGroup = new FormGroup({
+    nome: new FormControl('', [ Validators.required ]),
+    estado: new FormControl('', [ Validators.required ])
+  });
+
+  ngOnInit() {}
+
+  submitHandler(data) {
+    this.submitted = true;
+    console.log(this.formGroup.value)
+  }
 }
--- apps/ui/src/app/app.module.ts
+++ apps/ui/src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
